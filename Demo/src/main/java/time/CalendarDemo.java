66a0ede727ec42632dfbd1d41b2a8f45ef27939e
hhf <2856554735@qq.com>
2022-11-16 15:29:06 +0800

    "日期文件"

--- Demo/src/main/java/time/CalendarDemo.java
+++ Demo/src/main/java/time/CalendarDemo.java
@@ -1,5 +1,9 @@
 package time;
 
+import java.sql.Timestamp;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
 import java.util.Calendar;
 
 /**
@@ -10,7 +14,19 @@ import java.util.Calendar;
 public class CalendarDemo {
     public static void main(String[] args) {
         Calendar calendar = Calendar.getInstance();
+        System.out.println("calendar.getTime() = " + calendar.getTime());
         long time = calendar.getTime().getTime();
-        System.out.println(time);
+        System.out.println("calendar.getTime().getTime() = " + time);
+
+        Timestamp timestamp = new Timestamp(calendar.getTime().getTime());
+        System.out.println("timestamp = " + timestamp);
+
+        LocalDate now = LocalDate.now();
+        System.out.println("LocalDate = " + now);
+        LocalTime now1 = LocalTime.now();
+        System.out.println("LocalTime = " + now1);
+
+        LocalDateTime now2 = LocalDateTime.now();
+        System.out.println("LocalDateTime = " + now2);
     }
 }
