6203be6a3128213c2524ca59bca9aebb8b4fea5a
hhf <2856554735@qq.com>
2022-11-29 16:02:15 +0800

    打印流

---
+++ Demo/src/main/java/io/print/PrintStream_.java
@@ -0,0 +1,44 @@
+package io.print;
+
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.io.PrintStream;
+
+/**
+ * @Author hehongfei
+ * @Description
+ * @Date 2022/11/29 15:44
+ */
+public class PrintStream_ {
+    public static void main(String[] args) {
+
+    }
+    @Test
+    public void test() throws IOException {
+        PrintStream out = System.out;
+        //在默认情况下，PrintStream_ 输出数据的位置是 标准输出，即显示器
+        /*
+             public void print(String s) {
+                if (s == null) {
+                    s = "null";
+                }
+                write(s);
+            }
+         */
+        out.print("john, hello");
+        //因为print底层使用的是write , 所以我们可以直接调用write进行打印/输出
+        out.write("何洪飞,你好".getBytes());
+        out.close();
+
+        //我们可以去修改打印流输出的位置/设备
+        //1. 输出修改成到 "e:\\io\\f1.txt"
+        //2. "hello, 韩顺平教育~" 就会输出到 e:\\io\\f1.txt
+        //3. public static void setOut(PrintStream out) {
+        //        checkIO();
+        //        setOut0(out); // native 方法，修改了out
+        //   }
+        System.setOut(new PrintStream("e:\\io\\f1.txt"));
+        System.out.println("hello, 何洪飞~");
+    }
+}
