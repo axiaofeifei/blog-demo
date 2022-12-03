35e89f7b61d5da8f3eea5693edfc71761b0eab86
hhf <2856554735@qq.com>
2022-12-03 09:31:56 +0800

    git提交冲突测试

---
+++ Demo/src/main/java/gitTest/one/GitTestOne.java
@@ -0,0 +1,23 @@
+package gitTest.one;
+
+/**
+ * @Author hehongfei
+ * @Description
+ * @Date 2022/12/3 9:19
+ */
+//当你要提交一个文件的代码时，
+//另一个文件的GitTestTwo的代码与远程发生冲突
+    /*
+    * 1.先将GitTestOne的代码commit,然后pull，提示冲突。进行stash
+    * 两个文件本地会怎样，远程会怎样
+    * 然后 push
+    * 两个文件本地会怎样，远程会怎样
+    *
+    * 2.先将GitTestOne的代码commit,然后pull，提示冲突。进行merge
+    * 两个文件本地会怎样，远程会怎样
+    * 然后push
+    * 两个文件本地会怎样，远程会怎样
+    * */
+public class GitTestOne {
+
+}
---
+++ Demo/src/main/java/gitTest/one/GitTestTwo.java
@@ -0,0 +1,9 @@
+package gitTest.one;
+
+/**
+ * @Author hehongfei
+ * @Description
+ * @Date 2022/12/3 9:19
+ */
+public class GitTestTwo {
+}
