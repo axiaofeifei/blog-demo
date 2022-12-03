61f9642417e77bcb35fc376e73d82f78d22c7629
hhf <2856554735@qq.com>
2022-12-03 11:04:01 +0800

    git的test3

---
+++ Demo/src/main/java/gitTest/two/GitTest_3.java
@@ -0,0 +1,17 @@
+package gitTest.two;
+
+/**
+ * @Author hehongfei
+ * @Description
+ * @Date 2022/12/3 11:00
+ */
+
+/*
+* 如果一个代码，本地没有发生更改而远程发生了更改
+* 那么pull的时候，会怎样
+*
+* 1.要求你自己选择如何合并
+* 2.直接以远程的为准
+* */
+public class GitTest_3 {
+}
