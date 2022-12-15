0de152e8f308ee6b04b2f5e0fa4b944f879af950
hhf <2856554735@qq.com>
2022-12-15 15:26:38 +0800

    ip的工具类

--- utils/src/main/java/IpUtils.java
+++ utils/src/main/java/IpUtils.java
@@ -1,4 +1,11 @@
+import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+
 import javax.servlet.http.HttpServletRequest;
+import java.net.*;
+import java.util.Enumeration;
 
 /**
  * @author hehongfei
@@ -6,6 +13,145 @@ import javax.servlet.http.HttpServletRequest;
  */
 //获取客户端真实ip
 public class IpUtils {
+    private static final Logger LOGGER = LoggerFactory.getLogger(IpUtils.class);
+
+    private static final String UNKNOWN = "unknown";
+    private static final String LOCALHOST = "127.0.0.1";
+    private static final String SEPARATOR = ",";
+
+    private static final String HEADER_X_FORWARDED_FOR = "x-forwarded-for";
+    private static final String HEADER_PROXY_CLIENT_IP = "Proxy-Client-IP";
+    private static final String HEADER_WL_PROXY_CLIENT_IP = "WL-Proxy-Client-IP";
+
+    /**
+     * 获取真实客户端IP
+     * @param serverHttpRequest
+     * @return
+     */
+    public static String getRealIpAddress(ServerHttpRequest serverHttpRequest) {
+        String ipAddress;
+        try {
+            // 1.根据常见的代理服务器转发的请求ip存放协议，从请求头获取原始请求ip。值类似于203.98.182.163, 203.98.182.163
+            ipAddress = serverHttpRequest.getHeaders().getFirst(HEADER_X_FORWARDED_FOR);
+            if (StringUtils.isBlank(ipAddress) || UNKNOWN.equalsIgnoreCase(ipAddress)) {
+                ipAddress = serverHttpRequest.getHeaders().getFirst(HEADER_PROXY_CLIENT_IP);
+            }
+            if (StringUtils.isBlank(ipAddress) || UNKNOWN.equalsIgnoreCase(ipAddress)) {
+                ipAddress = serverHttpRequest.getHeaders().getFirst(HEADER_WL_PROXY_CLIENT_IP);
+            }
+
+
+            // 2.如果没有转发的ip，则取当前通信的请求端的ip
+            if (StringUtils.isBlank(ipAddress) || UNKNOWN.equalsIgnoreCase(ipAddress)) {
+                InetSocketAddress inetSocketAddress = serverHttpRequest.getRemoteAddress();
+                if(inetSocketAddress != null) {
+                    ipAddress = inetSocketAddress.getAddress().getHostAddress();
+                }
+                // 如果是127.0.0.1，则取本地真实ip
+                if (LOCALHOST.equals(ipAddress)  || "0:0:0:0:0:0:0:1".equals(ipAddress)) {
+                    InetAddress localAddress = getLocalHostLANAddress();
+                    if(localAddress.getHostAddress() != null) {
+                        ipAddress = localAddress.getHostAddress();
+                    }
+                }
+
+            }
+
+
+            // 对于通过多个代理的情况，第一个IP为客户端真实IP,多个IP按照','分割
+            // "***.***.***.***"
+            if (ipAddress != null) {
+                ipAddress = ipAddress.split(SEPARATOR)[0].trim();
+            }
+        } catch (Exception e) {
+            LOGGER.error("解析请求IP失败", e);
+            ipAddress = "";
+        }
+        return ipAddress == null ? "" : ipAddress;
+    }
+
+    /*
+     * 获取本机IP，方法一。
+     * */
+    public static String getRealIP(){
+        try {
+            Enumeration<NetworkInterface> allNetInterfaces = NetworkInterface
+                    .getNetworkInterfaces();
+            while (allNetInterfaces.hasMoreElements()) {
+                NetworkInterface netInterface = (NetworkInterface) allNetInterfaces
+                        .nextElement();
+
+                // 去除回环接口，子接口，未运行和接口
+                if (netInterface.isLoopback() || netInterface.isVirtual()
+                        || !netInterface.isUp()) {
+                    continue;
+                }
+
+                if (!netInterface.getDisplayName().contains("Intel")
+                        && !netInterface.getDisplayName().contains("Realtek")) {
+                    continue;
+                }
+                Enumeration<InetAddress> addresses = netInterface
+                        .getInetAddresses();
+                System.out.println(netInterface.getDisplayName());
+                while (addresses.hasMoreElements()) {
+                    InetAddress ip = addresses.nextElement();
+                    if (ip != null) {
+                        // ipv4
+                        if (ip instanceof Inet4Address) {
+                            System.out.println("ipv4 = " + ip.getHostAddress());
+                            return ip.getHostAddress();
+                        }
+                    }
+                }
+                break;
+            }
+        } catch (SocketException e) {
+            System.err.println("Error when getting host ip address"
+                    + e.getMessage());
+        }
+        return null;
+    }
+
+    /*
+     * 获取本机IP，方法二。
+     * */
+    private static InetAddress getLocalHostLANAddress() throws UnknownHostException {
+        try {
+            InetAddress candidateAddress = null;
+            // 遍历所有的网络接口
+            for (Enumeration ifaces = NetworkInterface.getNetworkInterfaces(); ifaces.hasMoreElements();) {
+                NetworkInterface iface = (NetworkInterface) ifaces.nextElement();
+                // 在所有的接口下再遍历IP
+                for (Enumeration inetAddrs = iface.getInetAddresses(); inetAddrs.hasMoreElements();) {
+                    InetAddress inetAddr = (InetAddress) inetAddrs.nextElement();
+                    if (!inetAddr.isLoopbackAddress()) {// 排除loopback类型地址
+                        if (inetAddr.isSiteLocalAddress()) {
+                            // 如果是site-local地址，就是它了
+                            return inetAddr;
+                        } else if (candidateAddress == null) {
+                            // site-local类型的地址未被发现，先记录候选地址
+                            candidateAddress = inetAddr;
+                        }
+                    }
+                }
+            }
+            if (candidateAddress != null) {
+                return candidateAddress;
+            }
+            // 如果没有发现 non-loopback地址.只能用最次选的方案
+            InetAddress jdkSuppliedAddress = InetAddress.getLocalHost();
+            if (jdkSuppliedAddress == null) {
+                throw new UnknownHostException("The JDK InetAddress.getLocalHost() method unexpectedly returned null.");
+            }
+            return jdkSuppliedAddress;
+        } catch (Exception e) {
+            UnknownHostException unknownHostException = new UnknownHostException(
+                    "Failed to determine LAN address: " + e);
+            unknownHostException.initCause(e);
+            throw unknownHostException;
+        }
+    }
 
 
     public static String getRemoteIP(HttpServletRequest request) {
