fbe2f91dc590c2f670facf22a00ecc0649547f9c
hhf <2856554735@qq.com>
2022-12-27 14:44:46 +0800

    说明

--- design_mode/src/main/java/behavior/strategy/sample_2/Duck.java
+++ design_mode/src/main/java/behavior/strategy/sample_2/Duck.java
@@ -7,7 +7,7 @@ package behavior.strategy.sample_2;
  */
 //策略模式第二个案例，这里我们用flyable接口和quackable接口实现飞行和叫功能
 //               如果这个鸭子会飞就实现flyable的接口,会叫就实现quackable的接口
-//
+//缺点:这样代码无法实现复用,子类要一边又一边的实现功能
 
 public abstract class Duck {
 
