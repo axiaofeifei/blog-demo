7e46dc505437adce6b89e6eb4e039bba9d61f202
hhf <2856554735@qq.com>
2022-12-28 10:44:32 +0800

    设计模式-观察者模式案例二-javaapi的理解

--- design_mode/src/main/java/behavior/observer/sample_2/WeatherData.java
+++ design_mode/src/main/java/behavior/observer/sample_2/WeatherData.java
@@ -12,6 +12,17 @@ import java.util.Observer;
  * @Date 2022/12/27 17:19
  */
 //使用JDK自带的api
+
+/*    你已经从我们的原则中得知这不是一件好事，但是，这到底会造成什么问题呢?
+            首先，因为0bservable是一个“类”，你必须设计一个类继承它。
+            如果某类想同时具有Observale类和另一个超类的行为，就会陷入两难，
+                毕竟Java不支持多重继承这限制了Observable的复用潜力(而增加复用潜力不正是我们使用模式最原始的动机吗?)
+            再者，因为没有0bservable接口，所以你无法建立自己的实现和Java内置的ObserverAPI搭配使用，
+            也无法将javautil的实现换成另一套做法的实现(比方说
+            Observable将关键的方法保护起来
+            如果你看看bservableAPI，你会发现setChanged(方法被保护起来了《被定义成protected)。那又怎么样呢?这意味着:除非你继承自0bservable，否则你无法创建Observable实例并组合到你自己的对象中来。
+            这个设计违反了第二个设计原则:“多用组合，少用继承”
+            */
 public class WeatherData extends Observable {
 
     private float temperature;
