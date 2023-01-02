eef556998b6137cd845d707cbfa34e6316d579f3
hhf <2856554735@qq.com>
2023-01-02 13:39:49 +0800

    设计模式-命令模式-灯

---
+++ design_mode/src/main/java/command/tool/GarageDoor.java
@@ -0,0 +1,16 @@
+package command.tool;
+
+/**
+ * @Author hehongfei
+ * @Description
+ * @Date 2023/1/2 9:27
+ */
+public class GarageDoor {
+    public void on(){
+        System.out.println("打开门...");
+    }
+
+    public void off(){
+        System.out.println("关闭门...");
+    }
+}
