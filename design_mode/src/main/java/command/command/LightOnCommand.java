0e18902940ff4f56bc5a3869a7bfa8a049b961ec
hhf <2856554735@qq.com>
2023-01-02 13:39:08 +0800

    设计模式-命令模式-灯的开关

---
+++ design_mode/src/main/java/command/command/LightOnCommand.java
@@ -0,0 +1,28 @@
+package command.command;
+
+
+import command.tool.Light;
+
+/**
+ * @Author hehongfei
+ * @Description
+ * @Date 2022/12/31 16:50
+ */
+public class LightOnCommand implements Command {
+
+    Light light;
+
+    public LightOnCommand(Light light) {
+        this.light = light;
+    }
+
+    @Override
+    public void execute() {
+        light.on();
+    }
+
+    @Override
+    public void undo() {
+        light.off();
+    }
+}
