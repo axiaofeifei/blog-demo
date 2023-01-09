67c8870d456dc78890190f74dca2a705672461dd
hhf <2856554735@qq.com>
2023-01-09 18:16:37 +0800

    设计模式

---
+++ design_mode/src/main/java/proxy/MatchMakingTestDrive.java
@@ -0,0 +1,27 @@
+package proxy;
+
+import org.junit.Test;
+
+/**
+ * @Author hehongfei
+ * @Description
+ * @Date 2023/1/9 15:40
+ */
+public class MatchMakingTestDrive {
+    public static void main(String[] args) {
+        MatchMakingTestDrive test = new MatchMakingTestDrive();
+        test.drive();
+    }
+
+    private void drive() {
+
+    }
+
+    public MatchMakingTestDrive(){
+        initializeDataBase();
+    }
+
+    private void initializeDataBase() {
+        
+    }
+}
