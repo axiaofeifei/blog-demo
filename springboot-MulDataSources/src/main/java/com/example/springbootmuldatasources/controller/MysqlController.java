e4390f2207c8f6b4a21d867eec2d048271d31aeb
hhf <2856554735@qq.com>
2023-01-26 17:28:38 +0800

    切换数据源查询数据

--- springboot-MulDataSources/src/main/java/com/example/springbootmuldatasources/controller/MysqlController.java
+++ springboot-MulDataSources/src/main/java/com/example/springbootmuldatasources/controller/MysqlController.java
@@ -1,5 +1,6 @@
 package com.example.springbootmuldatasources.controller;
 
+import com.baomidou.dynamic.datasource.annotation.DS;
 import com.example.springbootmuldatasources.entity.Dog;
 import com.example.springbootmuldatasources.service.DogService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,5 +23,16 @@ public class MysqlController {
     public void getOne(){
         Dog dog = dogService.getByName("小花");
         System.out.println(dog);
+    }
+
+    /**
+     * 测试多数据源通过，切换数据源查询数据
+     */
+    @GetMapping("/testDS")
+    @DS("mysql_2")
+    public Dog testDS(){
+        Dog dog = dogService.getByName("小黄");
+        System.out.println(dog);
+        return dog;
     }
 }
