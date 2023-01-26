f247ba784cdb6606337b60f24c602caf8a51980e
hhf <2856554735@qq.com>
2023-01-26 10:22:41 +0800

    satoken的启动类

--- springboot-satoken/src/main/java/com/example/springbootsatoken/SpringbootSatokenApplication.java
+++ springboot-satoken/src/main/java/com/example/springbootsatoken/SpringbootSatokenApplication.java
@@ -1,9 +1,12 @@
 package com.example.springbootsatoken;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication
+@MapperScan("com.example.springbootsatoken.mapper")
 public class SpringbootSatokenApplication {
 
     public static void main(String[] args) {
